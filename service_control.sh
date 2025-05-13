428e8c072ead150649586fad4b2b216b090a023e
skiiesea <amfin@internet.ru>
2025-05-13 10:21:58 +0300

    Add files via upload

--- service_control.sh
+++ service_control.sh
@@ -1,77 +1,53 @@
 #!/bin/bash
-if [ $? -ne 1 ]; then
-	echo "Use: $0 <service_name>"
-	exit 1
-fi
 
-service_name="$1"
+service=$1;
+s=$2;
+v=$3;
+vv=$4;
 
-if [ $? -ne 0 ]; then
-	echo "Service '$service_name' is not working. Reloading..."
-	if [ $? -eq 0 ]; then
-		echo "Service '$service_name' has been successfully launched"
-	else
-		echo "Coudn't restart the service '$service_name'"
-	fi
-else
-	echo "Service '$service_name' is working"
-fi
-
-count_lines(){
-	local path="$1"
-	local total_lines=0
-	local total_files=0
-
-	if [ -d "$path" ]; then
-		find "$path" -type f | while read -r file; do
-		if [ "$detail" = true ]; then
-			lines_in_file=$(wc -l < "$file")
-			echo "$lines_in_file $file"
-		fi
-		total_lines=$((total_lines + $(wc -l < "$file")))
-		total_files=$((total_files + 1))
-	done
-	elif [ -f  "$path" ]; then
-		if [ "$detail" = true ]; then
-			lines_in_file=$(wc -l < "$path")
-			echo "$lines_in_file $path"
-		fi
-		total_lines=$((total_lines + $(wc -l < "$path")))
-		total_files=1
-	else
-		echo "Specified path is unavailable: $path"
-		exit 1
-	fi
-
-	echo "Found $total_lines lines in $total_files file(s)"
+invalidArgs () {
+	echo "ERROR! Use this syntax: ./service_control <SERVICE_NAME> [OPTION] [OPTION]";
+	exit;
 }
 
-detail=false
-recursive=false
-target_path="$1"
-
-shift
-
-while [ $# -gt 0 ]; do
-	case "$1" in
-		-r)
-			recursive=true
-			;;
-		-detail)
-			detail=true
-			;;
-		*)
-			;;
-	esac
-	shift
-done
-
-if [ "$recursive" = true ] && [ -d "$target_path" ]; then
-	count_lines "$target_path"
-elif [ "$recursive" = false ]; then
-	count_lines "$target_path"
+if [ "$service" = "" ]; then
+	invalidArgs;
 else
-	echo "Functr -r required for counting recursively over a directory."
-	exit 1
-fi
+	if [ "$s" = "-s" ]; then
 
+		if [ "$v" = "" ]; then
+			invalidArgs;
+		else
+			action=$v;
+
+			if [ "$vv" = "-v" ]; then
+				more_info=true;
+			fi;
+		fi;
+
+
+	elif [ "$s" = "-v" ]; then
+		more_info=true;
+
+		if [ "$v" = "-s" ]; then
+                	if [ "$vv" = "" ]; then
+                        	invalidArgs;
+                	else
+                        	action=$vv;
+                	fi;
+		fi;
+	else
+		narg=true;
+	fi;
+fi;
+
+if !(systemctl is-active ${service}); then
+	sudo systemctl restart ${service};
+	echo "${service} restarted!";
+fi;
+
+if [ $action ]; then
+
+	sudo systemctl ${action} ${service};
+	echo "Action: ${action} for service: ${service} applied"
+fi;
